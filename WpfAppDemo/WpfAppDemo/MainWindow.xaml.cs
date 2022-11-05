19d8720a9d290ca4dc5fac3675805bedda0f7824
nmm <fox.334@mail.ru>
2022-11-05 11:43:31 +0300

    Частичное выполнение

--- WpfAppDemo/WpfAppDemo/MainWindow.xaml.cs
+++ WpfAppDemo/WpfAppDemo/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using WpfAppDemo.Classess;
 
 namespace WpfAppDemo
 {
@@ -21,9 +23,101 @@ namespace WpfAppDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Создание карточки объекта.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        public void CreateCard(int row, int column, string Text)
+        {
+            TextBlock text = new TextBlock();
+            text.Text = Text;
+            text.VerticalAlignment = VerticalAlignment.Bottom;
+            text.HorizontalAlignment = HorizontalAlignment.Center;
+            this.Content = text;
+        }
+
+        DBConnection database = new DBConnection();
+
         public MainWindow()
         {
             InitializeComponent();
+           
+        }
+
+        DataTable querty(string querty)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            SqlCommand command = new SqlCommand(querty, database.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return table;
+            }
+        }
+
+        private void Boreder_Move_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Shutdown_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void TAuth_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BAuth.Visibility = Visibility;
+            BShow.Visibility = Visibility.Hidden;
+        }
+
+        private void TShow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BAuth.Visibility = Visibility.Hidden;
+            BShow.Visibility = Visibility;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                  
+            DataTable obj = querty($"SELECT * FROM Auth WHERE Login = '{TB_Loggin.Text}' AND Pass = '{TB_Pass.Password}'");
+            if(obj.Rows.Count == 0)
+            {
+                MessageBox.Show("Пользователь не найден.");
+                return;
+            }
+
+            var user = new AuthModel(Convert.ToString(obj.Rows[0][0]), Convert.ToString(obj.Rows[0][1]), Convert.ToString(obj.Rows[0][2]), Convert.ToString(obj.Rows[0][3]));
+
+            if(user.Role == "Администратор")
+            {
+                var userW = new AdminWindow();
+                userW.Show();
+                this.Hide();
+            }
+            else if(user.Role == "Менеджер")
+            {
+                var userW = new ManagerWindow();
+                userW.Show();
+                this.Hide();
+            }
+            else if(user.Role == "Клиент")
+            {
+                var userW = new UserWindow();
+                userW.Show();
+                this.Hide();
+            }
+           
         }
     }
 }
---
+++ WpfAppDemo/WpfAppDemo/ManagerWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfAppDemo.Classess;
+
+namespace WpfAppDemo
+{
+    /// <summary>
+    /// Логика взаимодействия для ManagerWindow.xaml
+    /// </summary>
+    public partial class ManagerWindow : Window
+    {
+        DBConnection database = new DBConnection();
+
+        DataTable querty(string querty)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            SqlCommand command = new SqlCommand(querty, database.getConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                return table;
+            }
+        }
+
+        public void RefreshInfo()
+        {
+            DataTable obj = querty($"SELECT * FROM OrderModel");
+            var listStaff = new List<OrderModel>();
+            var set = 0;
+            while (set < obj.Rows.Count)
+            {
+                var staff = new OrderModel(Convert.ToInt32(obj.Rows[set][0]), Convert.ToString(obj.Rows[set][1]), Convert.ToDateTime(obj.Rows[set][2]), Convert.ToDateTime(obj.Rows[set][3]), Convert.ToInt32(obj.Rows[set][4]), Convert.ToString(obj.Rows[set][5]), Convert.ToString(obj.Rows[set][6]), Convert.ToString(obj.Rows[set][7]));
+                listStaff.Add(staff);
+                set++;
+            }
+            DataGrid_ShowInfo.ItemsSource = listStaff;
+        }
+
+        public ManagerWindow()
+        {
+            InitializeComponent();
+            RefreshInfo();
+        }
+
+        private void Button_Shutdown_Click(object sender, RoutedEventArgs e)
+        {
+            var main = new MainWindow();
+            main.Show();
+            this.Hide();
+        }
+
+        private void Boreder_Move_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        bool flag = false;
+        private void BUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            if(flag == false)
+            {
+                BUpdate.Background = Brushes.Pink;
+                DataGrid_ShowInfo.IsReadOnly = false;
+            }
+            else
+            {
+                BUpdate.Background = Brushes.LightGreen;
+                DataGrid_ShowInfo.IsReadOnly = true;
+            }
+            flag = !flag;
+            DataGrid_ShowInfo.Columns[0].IsReadOnly = true;
+        }
+
+        private void BEnd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var row_list = (OrderModel)DataGrid_ShowInfo.SelectedValue;
+
+            }
+            catch
+            {
+                MessageBox.Show("Не выбран элемент изменения");
+                return;
+            }
+            try
+            {
+                var lo = (OrderModel)DataGrid_ShowInfo.SelectedValue;
+                DataTable obj = querty($"UPDATE OrderModel SET Order_composition = '{lo.Order_composition}', Order_date = '{lo.Order_date}', Delivery_date = '{lo.Delivery_date}',point_of_issue = '{lo.Point_of_issue}', userName = '{lo.UserName}',code_to_get = '{lo.Code_to_get}',status_Order = '{lo.Status_Order}'  WHERE id = {lo.Id} ");
+
+            }
+            catch
+            {
+                MessageBox.Show("Изменение не успешно, проверте правильность вводимых данных");
+                return;
+            }
+            RefreshInfo();
+
+        }
+    }
+}
